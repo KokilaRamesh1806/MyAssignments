cbd26b1a0300945c5f8d16f76cca01acf272fae6
KokilaRamesh1806 <kokilat@gmail.com>
2025-10-16 10:13:41 +0530

    Add files via upload

---
+++ Day1/Datatype.js
@@ -0,0 +1,13 @@
+var emp = 'Kokila'
+var company = 'Hexaware'
+var mobno = 999998888
+var isAutomation = true
+var hasplaywright
+
+console.log("Type of emp is " + typeof(emp) + " " + "and the value is "+ emp)
+console.log("Type of company is " + typeof(company) + " " + "and the value is "+ company)
+console.log("Type of mobno is " + typeof(mobno) + " " + "and the value is "+ mobno)
+console.log("Type of isAutomation is " + typeof(isAutomation) + " " + "and the value is "+ isAutomation)
+console.log("Type of hasplaywright is " + typeof(hasplaywright) + " " + "and the value is "+ hasplaywright )
+
+
